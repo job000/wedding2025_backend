a17553311b477613adc8cccb96e74da72c0794fd
job000 <obligar@live.no>
2025-01-07 02:40:00 +0100

    Tested API calls. working.

--- app/config.py
+++ app/config.py
@@ -6,13 +6,13 @@ class Config:
     SECRET_KEY = 'supersecretkey'  # Hardkodet verdi for lokal utvikling
     # Database
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL',
-        'postgresql://wedding:password@postgres_db:5432/wedding_db'
-    )
+    'DATABASE_URL',
+    'postgresql://wedding2025_db_user:password@dpg-ctu8901u0jms73f52hs0-a:5432/wedding2025_db'
+)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # JWT
     #JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwtsecretkey')
-    JWT_SECRET_KEY = "supersecretkey"
+    JWT_SECRET_KEY = "jwtsecretkey"
 
     JWT_ACCESS_TOKEN_EXPIRES = 24 * 60 * 60  # 24 hours
