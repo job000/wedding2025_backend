9a7faffc9c4528d92fbfa6292de1ad6b6ae178c0
job000 <obligar@live.no>
2025-01-09 01:10:54 +0100

    Test.

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,10 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
     
+    # Konfigurer CORS
+    origins = ["http://localhost:3000", "https://fridaandjohnmichaelwedding.onrender.com"]
+    CORS(app, resources={r"/*": {"origins": origins}})
+    
     # Logg JWT_SECRET_KEY
     print(f"JWT_SECRET_KEY: {app.config['JWT_SECRET_KEY']}")
 
